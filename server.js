f3cac846d365e16eb44e21c6f2a12c8f17f8ed46
tehvicke <vikda574@student.liu.se>
2020-02-07 10:37:58 +0100

    added pagination and refactored the query buildup a bit

--- server.js
+++ server.js
@@ -1,96 +1,125 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const MAXPERPAGE = 10 /* max results per page */
+const MAXPERPAGE = 10; /* max results per page */
 
 // import goldenGlobesData from './data/golden-globes.json'
-import avocadoSalesData from './data/avocado-sales.json'
+import avocadoSalesData from "./data/avocado-sales.json";
 // import booksData from './data/books.json'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-
-const AvocadoSales = mongoose.model('AvocadoSales', {
+const AvocadoSales = mongoose.model("AvocadoSales", {
   id: Number,
-  date: Date, /* Date is in string in json but is automatically converted. Time is set to 00:00:00 however */
+  date: Date /* Date is in string in json but is automatically converted. Time is set to 00:00:00 however */,
+  averagePrice: Number,
   totalVolume: Number,
   totalBagsSold: Number,
   smallBagsSold: Number,
   largeBagsSold: Number,
   xLargeBagsSold: Number,
   region: String
-})
+});
 
 if (process.env.RESET_DB) {
-	const seedDatabase = async () => {
-    await AvocadoSales.deleteMany({})
+  const seedDatabase = async () => {
+    await AvocadoSales.deleteMany({});
 
-		avocadoSalesData.forEach((item) => {
-			new AvocadoSales(item).save()
-		})
-  }
+    avocadoSalesData.forEach(item => {
+      new AvocadoSales(item).save();
+    });
+  };
 
-  seedDatabase()
-  console.log("Database seeded")
+  seedDatabase();
+  console.log("Database seeded");
 }
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start defining your routes here
-app.get('/avocado-sales', async (req, res) => {
-  const { /* destructure the query params */
-    start, 
-    end, 
-    page = 1,
+const queryBuilder = query => {
+  const {
+    /* destructure the query params */
+    start,
+    end,
     "average-price-min": averagePriceMin,
-    "average-price-max": averagePriceMax
-    } = req.query
+    "average-price-max": averagePriceMax,
+    "total-volume-min": totalVolumeMin,
+    "total-volume-max": totalVolumeMax
+  } = query;
 
+  /* Error handling */
+  if (start === undefined || end === undefined) {
     /* Ensure we get dates, send 400 error otherwise */
-    if(start === undefined || end === undefined) {
-      return res.status(400).json({
-          status:'failure',
-          message: 'Please ensure you pick two dates'
-           })
-          }
-  
-
-
-    let sales = await AvocadoSales.find({ 
-      date: { /* check for date range greater or equal && lesser than */ 
-            $gte: new Date(start),
-            $lt: new Date(end)
-             }
-      }).sort({ date: 'desc'})  /** TODO: Add pagination in database and not in response - preferably without Skip() as it's resource intensive */
-    
-    /* Handle pages */
-    const response = sales.slice((page - 1) * MAXPERPAGE, page * MAXPERPAGE)
-    const numberOfPages = Math.ceil(sales.length / MAXPERPAGE)
-  res.json(
-    {
-      'number-of-pages': numberOfPages,
-      'current-page': page, /* not really necessary as the client should know this but could be good for confirmation... or something.. */
-      'data': response
+    return res.status(400).json({
+      status: "failure",
+      message: "Please ensure you pick two dates"
+    });
+  }
+  /* Add mandatory fields to the query */
+  const dbQuery = {
+    date: {
+      /* check for date range greater or equal && lesser than */
+      $gte: new Date(start),
+      $lt: new Date(end)
     }
-  )
-})
+  };
+  /* Adds optional fields */
+  if (averagePriceMin || averagePriceMax) {
+    dbQuery["averagePrice"] = {};
+    if (averagePriceMin) {
+      dbQuery["averagePrice"]["$gte"] = +averagePriceMin;
+    }
+    if (averagePriceMax) {
+      dbQuery["averagePrice"]["$lte"] = +averagePriceMax;
+    }
+  }
+  if (totalVolumeMin || totalVolumeMax) {
+    dbQuery["totalVolume"] = {};
+    if (averagePriceMin) {
+      dbQuery["totalVolume"]["$gte"] = +totalVolumeMin;
+    }
+    if (averagePriceMax) {
+      dbQuery["totalVolume"]["$lte"] = +totalVolumeMax;
+    }
+  }
+
+  console.log(dbQuery);
+  return dbQuery;
+};
+// Start defining your routes here
+app.get("/avocado-sales", async (req, res) => {
+  const page = req.query.page || 1;
+
+  const response = await AvocadoSales.find(queryBuilder(req.query))
+    .skip(MAXPERPAGE * (page - 1))
+    .limit(MAXPERPAGE);
+
+  const numberOfPages = Math.ceil(
+    (await AvocadoSales.countDocuments(dbQuery)) / MAXPERPAGE
+  ); /* Not really needed to send for each new page request - could store in frontend and pass bool to not run this function */
+  res.json({
+    "number-of-pages": numberOfPages,
+    "current-page": +page /* not really necessary as the client should know this but could be good for confirmation... or something.. */,
+    data: response
+  });
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
